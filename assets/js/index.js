0b63a8e49713846a540d94dd1793775b3bf137d8
Aamer-Qanadilo <aamer_qanadilo@hotmail.com>
2023-05-01 00:14:45 +0300

    Implemented Delete and Mark As Done Buttons

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,7 @@ const inputTask = document.getElementById("task");
 const inputAssignee = document.getElementById("assignee");
 const todoListForm = document.getElementById("todoListForm");
 const inputFields = document.querySelectorAll("#todoListForm input");
+const TodoListContainer = document.querySelector(".TodoListContainer");
 
 // buttons
 const addTaskBtn = document.getElementById("addTask");
@@ -26,16 +27,20 @@ let inputsError = [];
 // Decelerations for the application
 
 let tasks = [];
+// const filters = { All: "" };
 
 // Start Implementing Functionalities
 
 const updateLocalStorage = () => {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+  displayTasks();
 };
 
 const fetchLocalStorage = () => {
   const LocalTasks = JSON.parse(localStorage.getItem("tasks"));
   if (LocalTasks) tasks = LocalTasks;
+
+  displayTasks();
 };
 
 window.addEventListener("load", () => {
@@ -47,6 +52,19 @@ todoListForm.addEventListener("keyup", (event) => {
   if (event.target.tagName !== "INPUT") return;
 
   checkInput(event.target);
+});
+
+TodoListContainer.addEventListener("click", (event) => {
+  if (event.target.tagName !== "I") return;
+
+  const taskCard = event.target.closest(".taskCard");
+  const { ariaLabel } = event.target;
+
+  if (ariaLabel == "Delete task") {
+    deleteTask(taskCard);
+  } else if (ariaLabel == "Toggle Task Completed") {
+    toggleDone(taskCard);
+  }
 });
 
 function checkInput(input) {
@@ -118,7 +136,79 @@ const addTask = () => {
   tasks.push(task);
 };
 
-addTaskBtn.onclick = function () {
+const deleteTask = (taskCardNode) => {
+  const parentNode = taskCardNode.parentElement;
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Delete!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const index = taskCardNode.ariaRowIndex;
+      parentNode.removeChild(taskCardNode);
+      tasks.splice(index, 1);
+
+      updateLocalStorage();
+
+      Swal.fire("Deleted!", "Your file has been deleted.", "success");
+    }
+  });
+};
+
+const toggleDone = (taskCardNode) => {
+  const index = taskCardNode.ariaRowIndex;
+  tasks[index].completed = !tasks[index].completed;
+  updateLocalStorage();
+};
+
+const displayTasks = () => {
+  let tasksCards = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+    tasksCards += `
+        <div class="taskCard" aria-rowindex="${i}">
+          <div class="taskCardContent">
+            <p class="${
+              tasks[i].completed
+                ? "opacity-50 text-decoration-line-through"
+                : ""
+            }">${tasks[i].task}</p>
+            <p class="${
+              tasks[i].completed
+                ? "opacity-50 text-decoration-line-through"
+                : ""
+            }">${tasks[i].assignee}</p>
+          </div>
+          <div class="taskCardButtons">
+            <i
+              class="fa-solid fa-trash"
+              style="color: #df0c0c"
+              aria-label="Delete task"
+            ></i>
+            <i 
+                class='${
+                  tasks[i].completed
+                    ? "fa-solid fa-circle-xmark"
+                    : "fa-solid fa-circle-check"
+                }'
+                style='${
+                  tasks[i].completed ? "color: #df0c0c" : "color: #207e44"
+                }'
+                aria-label="Toggle Task Completed"
+            ></i>
+          </div>
+        </div>`;
+  }
+
+  TodoListContainer.innerHTML = tasksCards;
+};
+
+addTaskBtn.onclick = function (event) {
+  event.preventDefault();
   checkAllInputs();
 
   if (inputsError.length !== 0) return;
